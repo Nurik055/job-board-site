c067c20e8c9847d2a05d45f3353ac32f8ec42d4e
Nursultan <nurty1757@gmail.com>
2026-05-17 00:11:57 +0500

    Pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,14 +1,22 @@
-function Pagination({ currentPage, setCurrentPage, totalPages }) {
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+function Pagination({totalPages, currentPage, setCurrentPage}) {
 
-  return (
+  const pages = Array.from( {length: totalPages}, (_, i) => i+1);
+
+
+  return(
     <div>
-      {pages.map((page) => (
-        <button key={page} onClick={() => setCurrentPage(page)}>
-          {page}
-        </button>
-      ))}
+    {pages.map((page) =>(
+      <button 
+      key={page},
+      onClick={() => setCurrentPage(page)}
+      >
+        {page}
+      </button>
+    ))}
     </div>
-  );
+  )
+
 }
+
+
 export default Pagination;
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,11 @@
 import { AuthContext } from "../../context/AuthContext";
 import { useContext } from "react";
-import {user} from 
 
 
 function Profile() {
     return(
         <div>
-            <h1>Profile</h1>
-            <h3>Name: {user.name}</h3>
+           
         </div>
     )
 }
